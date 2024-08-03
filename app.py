cecdeb1d8f4e67430c2f1e73e15b9b8e9e58f1a4
isaiRDZA <167360337+isaiRDZA@users.noreply.github.com>
2024-08-03 05:59:00 +0000

    Changing to correct code

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def predict(prompt):
     return summary
 
 with gr.Blocks() as demo:
-    textbox = ge.textbox(placeholder = "Enter text block to summarize", lines = 4)
+    textbox = gr.Textbox(placeholder = "Enter text block to summarize", lines = 4)
     gr.Interface(fn=predict, inputs = textbox, outputs = "text")
 
 
